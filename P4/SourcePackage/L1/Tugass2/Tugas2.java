1d05e24c23f208207ac5d78ba4134c82e8e68ca1
Asyhadullah <arsadullah39@gmail.com>
2026-04-03 15:06:31 +0700

    update

---
+++ P4/SourcePackage/L1/Tugass2/Tugas2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Tugas2 {
+   public static void main(String[] args) {
+    Scanner inpuScanner = new Scanner(System.in);
+
+    System.out.println(" Progam Menghitung Luas dan keliling Matematika ");
+
+    System.out.println(" Masukkan Jari- Jari");
+    double r = input.nextDouble();
+
+    double luas = Math.PI *r *r;
+    double keliling = 2 * Math.PI *r;
+
+    System.out.println(" Luas Linkaran: " + luas);
+    System.out.println(" Keliling Lingkaran: "+ keliling);
+   }    
+}
