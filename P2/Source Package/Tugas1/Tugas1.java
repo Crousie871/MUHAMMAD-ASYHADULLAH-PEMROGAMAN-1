14b5bc1ee5f82956bb2f10eea12c37e92b8a9618
Asyhadullah <arsadullah39@gmail.com>
2026-04-03 15:26:44 +0700

    commit update

--- P2/Source Package/Tugas1/Tugas1.java
+++ P2/Source Package/Tugas1/Tugas1.java
@@ -3,33 +3,29 @@ import java.util.Scanner;
 
 public class Tugas1 {
     public static void main(String[] args) {
-
-        try (Scanner input = new Scanner(System.in)) {
-            System.out.println("=== Form Data Mahasiswa ===");
+        Scanner input = new Scanner(System.in);
+        System.out.println("=== Form Data Mahasiswa ===");
             
-            System.out.print("Masukkan Nama: ");
-            String nama = input.nextLine();
+        System.out.print("Masukkan Nama: ");
+        String nama = input.nextLine();
 
            
-            System.out.print("Masukkan Umur: ");
-            int usia = input.nextInt();
+        System.out.print("Masukkan Umur: ");
+        int usia = input.nextInt();
+        input.nextLine();
 
-            System.out.print("Masukkan Universitas: ");
-            String universitas = input.nextLine();
+        System.out.print("Masukkan Universitas: ");
+        String universitas = input.nextLine();
 
-            System.out.print("Masukkan Jurusan/Prodi: ");
-            String jurusan = input.nextLine();
+        System.out.print("Masukkan Jurusan/Prodi: ");
+        String jurusan = input.nextLine();
 
-            System.out.println("\n--------------------");
-            System.out.println("Informasi Mahasiswa:");
-            System.out.println("Nama        : " + nama);
-            System.out.println("Umur        : " + usia + " tahun");
-            System.out.println("Universitas : " + universitas);
-            System.out.println("Jurusan     : " + jurusan);
-            System.out.println("--------------------");
-
-        } catch (Exception e) {
-            System.out.println("Terjadi kesalahan sistem: " + e.getMessage());
-        }
+        System.out.println("\n--------------------");
+        System.out.println("Informasi Mahasiswa:");
+        System.out.println("Nama        : " + nama);
+        System.out.println("Umur        : " + usia + " tahun");
+        System.out.println("Universitas : " + universitas);
+        System.out.println("Jurusan     : " + jurusan);
+        System.out.println("--------------------");
     }
 }
