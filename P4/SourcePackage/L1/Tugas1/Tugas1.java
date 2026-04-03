e02bba973d6884d706f08b1ae3c7828f00b16fb5
Asyhadullah <arsadullah39@gmail.com>
2026-04-03 15:19:43 +0700

    update

--- P4/SourcePackage/L1/Tugas1/Tugas1.java
+++ P4/SourcePackage/L1/Tugas1/Tugas1.java
@@ -1,5 +1,30 @@
-package Tugas1;
-
+import java.util.Scanner;
 public class Tugas1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double makan, transport, belanja, total, PersenMakan, PersenTransport, PersenBelanja;
+
+        System.out.println(" Masukkan biaya makan:");
+        makan = input.nextDouble();
+
+        System.out.println(" Masukkan biaya transport:");
+        transport  = input.nextDouble();
+
+        System.out.println(" Masukkan biaya belanja");
+        belanja = input.nextDouble();
+
+        total = makan + transport + belanja;
+        PersenMakan = ( makan * 100) / total;
+        PersenTransport = ( transport * 100 ) / total;
+        PersenBelanja = ( belanja *100)/ total;
+
+        System.out.println("\n Total" + total);
+        System.out.println(" Presentase: ");
+        System.out.println(" Makan = " + PersenMakan + "%");
+        System.out.println(" Transport = " + PersenTransport + "%");
+        System.out.println(" Belanja = " + PersenBelanja + "%");
+
+     
+    }
     
 }
