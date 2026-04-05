e3190b68ed373d7983137b5df3bd450af6bb7653
Asyhadullah <arsadullah39@gmail.com>
2026-04-05 14:30:34 +0700

    Update Tugas1.java

--- P4/SourcePackage/Tugas1/Tugas1.java
+++ P4/SourcePackage/Tugas1/Tugas1.java
@@ -4,6 +4,7 @@ public class Tugas1 {
         Scanner input = new Scanner(System.in);
         double makan, transport, belanja, total, PersenMakan, PersenTransport, PersenBelanja;
 
+        System.out.println(" Hari ke 1");
         System.out.println(" Masukkan biaya makan:");
         makan = input.nextDouble();
 
@@ -23,6 +24,29 @@ public class Tugas1 {
         System.out.println(" Makan = " + PersenMakan + "%");
         System.out.println(" Transport = " + PersenTransport + "%");
         System.out.println(" Belanja = " + PersenBelanja + "%");
+
+        double makan2, transport2, belanja2, total2, PersenMakan2, PersenTransport2, PersenBelanja2;
+
+        System.out.println(" Hari ke 2");
+        System.out.println(" Masukkan biaya makan:");
+        makan2 = input.nextDouble();
+
+        System.out.println(" Masukkan biaya transport:");
+        transport2  = input.nextDouble();
+
+        System.out.println(" Masukkan biaya belanja");
+        belanja2 = input.nextDouble();
+
+        total2 = makan2 + transport2 + belanja2;
+        PersenMakan2 = ( makan2 * 100) / total2;
+        PersenTransport2 = ( transport2 * 100 ) / total2;
+        PersenBelanja2 = ( belanja2 *100)/ total2;
+
+        System.out.println("\n Total" + total2);
+        System.out.println(" Presentase: ");
+        System.out.println(" Makan = " + PersenMakan2 + "%");
+        System.out.println(" Transport = " + PersenTransport2 + "%");
+        System.out.println(" Belanja = " + PersenBelanja2 + "%");
 
      
     }
